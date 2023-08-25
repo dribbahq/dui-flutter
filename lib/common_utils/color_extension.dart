32bd0d851b8c242fa6eabadc306ec18a35a93ee4
Alex Tarragó <alex@dribba.com>
2023-08-25 09:37:06 +0200

    feature(update): add animations.

--- lib/common_utils/color_extension.dart
+++ lib/common_utils/color_extension.dart
@@ -10,4 +10,17 @@ extension ColorExtension on Color {
         ? Dalai.color.text
         : Dalai.color.textDarkMode;
   }
+
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+      '${alpha.toRadixString(16).padLeft(2, '0')}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
 }
